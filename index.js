dc394a1f32ad75f49e0807129cdc0fb1bb2a2149
boomyville <boomyville@gmail.com>
2023-11-19 22:26:47 +1100

    Update index.js

--- index.js
+++ index.js
@@ -29,4 +29,4 @@ function play(playerInput) {
 }
 
 let prompt = window.prompt("Rock Paper or Scissors?");
-document.getElementById("result").innerHTML = play(prompt.toUpperCase());
+document.getElementById("game").innerHTML = play(prompt.toUpperCase());
