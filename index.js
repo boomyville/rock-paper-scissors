9483b07cb5f8d788472348ff84d9b38917ef8b68
boomyville <boomyville@gmail.com>
2023-11-19 12:59:33 +1100

    Update index.js

--- index.js
+++ index.js
@@ -5,24 +5,26 @@ function getComputerChoice() {
   return options[Math.floor(Math.random() * options.length)];
 }
 
-function playerChoice(input) {
-  return input.toUpperCase();
-}
-
-function play(playerInput, computerInput) {
+function play(playerInput) {
+  let computerInput = getComputerChoice();
   switch(playerInput) {
     case "ROCK":
       if(computerInput == "ROCK") return "Draw"
       if(computerInput == "SCISSORS") return "Win"
       if(computerInput == "PAPER") return "Lose"
+      break;
     case "SCISSORS":
       if(computerInput == "ROCK") return "Lose"
       if(computerInput == "SCISSORS") return "Draw"
       if(computerInput == "PAPER") return "Win"
+      break;
     case "PAPER":
       if(computerInput == "ROCK") return "Win"
       if(computerInput == "SCISSORS") return "Lose"
       if(computerInput == "PAPER") return "Draw"
+      break;
+    default: 
+      return "Invalid"
   }
 }
 
